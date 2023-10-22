e7eef14babbcb6fd18e661ab26e7bff01f02e2d6
flaviadanielareisenauer <flaviareisenauer@gmail.com>
2023-10-22 00:48:06 -0300

    ejercicio 1 suma simple

---
+++ src/Problemas1/Ejercicio1_SumaSimple.java
@@ -0,0 +1,13 @@
+package Problemas1;
+
+import java.util.Scanner;
+
+public class Ejercicio1_SumaSimple {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int a;
+        a = entrada.nextInt();
+
+        System.out.println(a + 1);
+    }
+}
