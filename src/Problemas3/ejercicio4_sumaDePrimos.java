55e9af551827aa6af7e151ddee85b3e0bb05a1d4
Flavia Daniela Reisenauer <85591614+flaviadanielareisenauer@users.noreply.github.com>
2023-10-23 22:25:11 -0300

    Update ejercicio4_sumaDePrimos.java

--- src/Problemas3/ejercicio4_sumaDePrimos.java
+++ src/Problemas3/ejercicio4_sumaDePrimos.java
@@ -1,20 +1,43 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package Problemas3;
 
-/**
- *
- * @author Flavia
- */
-public class ejercicio4_sumaDePrimos {
+import java.util.Scanner;
 
-    /**
-     * @param args the command line arguments
-     */
+public class ejercicio4_sumaDePrimos {
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        if (!esPrimo(n)) {
+            int[] primos = encontrarSumaDosPrimos(n);
+            System.out.println(primos[0] + " " + primos[1]);
+        } else {
+			System.out.println(n + " 0");
+        }
+
+        scanner.close();
     }
-    
+
+    public static boolean esPrimo(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static int[] encontrarSumaDosPrimos(int n) {
+        int[] primos = new int[2];
+        for (int i = 2; i <= n / 2; i++) {
+            if (esPrimo(i) && esPrimo(n - i)) {
+                primos[0] = Math.max(i, n - i);
+                primos[1] = Math.min(i, n - i);
+                break;
+            }
+        }
+        return primos;
+    }
 }
