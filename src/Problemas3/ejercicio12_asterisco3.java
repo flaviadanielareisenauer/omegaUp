ef07ac692b00fc0bafe9c3ad573d288617d7add9
Flavia Daniela Reisenauer <85591614+flaviadanielareisenauer@users.noreply.github.com>
2023-10-23 22:50:42 -0300

    Update ejercicio12_asterisco3.java

--- src/Problemas3/ejercicio12_asterisco3.java
+++ src/Problemas3/ejercicio12_asterisco3.java
@@ -1,20 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package Problemas3;
 
-/**
- *
- * @author Flavia
- */
-public class ejercicio12_asterisco3 {
+import java.util.Scanner;
 
-    /**
-     * @param args the command line arguments
-     */
+public class ejercicio12_asterisco3 {
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        if (n < 1 || n > 100) {
+            System.out.println("Por favor, ingrese un valor en el rango permitido.");
+        } else {
+            for (int i = 1; i <= n; i++) {
+                for (int j = 0; j < n - i; j++) {
+                    System.out.print(" "); 
+                }
+
+                for (int j = 0; j < 2 * i - 1; j++) {
+                    System.out.print("*");
+                }
+
+                System.out.println(); 
+            }
+        }
+        scanner.close();
     }
-    
 }
