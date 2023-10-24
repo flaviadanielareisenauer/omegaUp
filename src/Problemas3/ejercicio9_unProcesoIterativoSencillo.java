2092b1720aad3c04f1cdebb7b18d518cda04c36b
Flavia Daniela Reisenauer <85591614+flaviadanielareisenauer@users.noreply.github.com>
2023-10-23 22:49:30 -0300

    Update ejercicio9_unProcesoIterativoSencillo.java

--- src/Problemas3/ejercicio9_unProcesoIterativoSencillo.java
+++ src/Problemas3/ejercicio9_unProcesoIterativoSencillo.java
@@ -1,20 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package Problemas3;
 
-/**
- *
- * @author Flavia
- */
-public class ejercicio9_unProcesoIterativoSencillo {
+import java.util.Scanner;
 
-    /**
-     * @param args the command line arguments
-     */
+public class ejercicio9_unProcesoIterativoSencillo {
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+
+        if (n < 1) {
+            System.out.println("Por favor, ingrese un valor de N mayor o igual a 1.");
+        } else {
+            while (n < 100) {
+                if (n % 2 == 0) {
+                    n += 3;
+                } else {
+                    n *= 2;
+                }
+            }
+
+            System.out.println(n);
+        }
+
+        scanner.close();
     }
-    
 }
